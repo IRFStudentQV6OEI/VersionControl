b6289ed23a973cd60905c9755a40615972b71ab2
Benjamin Ben Driss <bendriss.beni32@gmail.com>
2022-10-14 08:35:01 +0200

    6.feladat

--- Week04/Week04/Form1.cs
+++ Week04/Week04/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
+using System.Data.Entity.Migrations.Model;
 
 namespace Week04
 {
@@ -14,16 +17,69 @@ namespace Week04
     {
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> Flats;
+        Excel.Application xlApp;
+        Excel.Workbook xlWb;
+        Excel.Worksheet xlWs;
              
         public Form1()
         {
             InitializeComponent();
             Loaddata();
+            CreateExcel();
+        }
+
+        private void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWb = xlApp.Workbooks.Add(Missing.Value);
+                xlWs = xlWb.ActiveSheet;
+                CreateTable();
+                xlApp.Visible = true;
+                xlWb.UserControl = true;
+
+            }
+            catch (Exception ex)
+            {
+                String errmsg = String.Format("Error: {0}\nline: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errmsg,"Error");
+                xlWb.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWb = null;
+                xlApp = null;
+            }
+        }
+
+        private void CreateTable()
+        {
+            throw new NotImplementedException();
         }
 
         private void Loaddata()
         {
-            Flats = context.Flats.ToList();
+            var Headers = new string[]
+            {
+                "Kód",
+                "Eladó",
+                "Oldal",
+                "Kerület",
+                "Lift",
+                "Szobák száma",
+                "Alapterület (m2)",
+                "Ár (Mft)",
+                "Négyzetméter ár (Ft/m2)"
+            };
+            for (int i = 0; i < Headers.Length; i++)
+            {
+                xlWs.Cells[1, i + 1] = Headers[i];
+            }
+            object[,] values = new object[Flats.Count, Headers.Length];
+            int counter = 0;
+            foreach (var f in Flats)
+            {
+
+            }
         }
     }
 }
