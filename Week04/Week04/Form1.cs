63258c1d6649da2cbecee98b23aeadbd91f2e7da
Benjamin Ben Driss <bendriss.beni32@gmail.com>
2022-10-14 08:15:58 +0200

    4.Feladat

--- Week04/Week04/Form1.cs
+++ Week04/Week04/Form1.cs
@@ -12,9 +12,18 @@ namespace Week04
 {
     public partial class Form1 : Form
     {
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> Flats;
+             
         public Form1()
         {
             InitializeComponent();
+            Loaddata();
+        }
+
+        private void Loaddata()
+        {
+            Flats = context.Flats.ToList();
         }
     }
 }
