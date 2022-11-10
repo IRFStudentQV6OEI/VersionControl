f37b3511effe6529d6e67797b7fd09d7918fa2f8
Benjamin Ben Driss <bendriss.beni32@gmail.com>
2022-11-10 20:02:46 +0100

    fájlba írás

--- VaR/VaR/Form1.cs
+++ VaR/VaR/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,12 +16,34 @@ namespace VaR
         PortfolioEntities context = new PortfolioEntities();
         List<Tick> Ticks;
         List<PortfolioItem> Portfolio = new List<PortfolioItem>();
+        List<decimal> rendezettNyereségek = new List<decimal>();
         public Form1()
         {
             InitializeComponent();
             Ticks = context.Ticks.ToList();
             dataGridView1.DataSource = Ticks;
             CreatePortfolio();
+            List<decimal> Nyereségek = new List<decimal>();
+            int intervalum = 30;
+            DateTime kezdőDátum = (from x in Ticks select x.TradingDay).Min();
+            DateTime záróDátum = new DateTime(2016, 12, 30);
+            TimeSpan z = záróDátum - kezdőDátum;
+            for (int i = 0; i < z.Days - intervalum; i++)
+            {
+                decimal ny = GetPortfolioValue(kezdőDátum.AddDays(i + intervalum))
+                           - GetPortfolioValue(kezdőDátum.AddDays(i));
+                Nyereségek.Add(ny);
+                Console.WriteLine(i + " " + ny);
+            }
+
+            var  nyereségekRendezve = (from x in Nyereségek
+                                      orderby x
+                                      select x)
+                                        .ToList();
+            rendezettNyereségek = nyereségekRendezve;
+            MessageBox.Show(nyereségekRendezve[nyereségekRendezve.Count() / 5].ToString());
+            
+
         }
 
         private void CreatePortfolio()
@@ -43,6 +66,20 @@ namespace VaR
                 value += (decimal)last.Price * item.Volume;
             }
             return value;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() != DialogResult.OK) return;
+                using (StreamWriter sw = new StreamWriter(sfd.FileName,false,Encoding.UTF8))
+            {
+                sw.WriteLine("Időszak, Nyereség");
+                for (int i = 0; i < rendezettNyereségek.Count(); i++)
+                {
+                    sw.WriteLine(i+1.ToString() +", "+ rendezettNyereségek[i].ToString());
+                }
+            }
         }
     }
 }
